36433a21c37bd1f70e755e142b34ab0f82bd4f7a
jolega <jolegacm@gmail.com>
2024-11-25 06:04:33 -0500

    update desctructuring

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,52 @@
+
+
+interface Product {
+    descrption : string;
+    price: number;
+}
+
+const phone: Product = {
+    descrption : 'Nokia A1',
+    price: 150.0,
+}
+
+const tablet: Product = {
+    descrption : 'ipad Air',
+    price: 250.0,
+}
+
+
+interface taxCalculationOption {
+    tax: number;
+    products: Product []
+}
+function taxCalculation (options: taxCalculationOption ):  [number, number] {
+
+    let total= 0
+    const { tax, products } = options
+
+
+    products.forEach( ({ price }) => {
+
+        total += price
+        
+    })
+
+    return [total, total * tax]
+
+}
+
+const shoopingCart = [phone, tablet];
+const tax = 0.15
+
+const [ total , totalWithTax ] = taxCalculation ({
+    products: shoopingCart,
+    tax: tax,
+}
+)
+
+console.log('total', total)
+console.log('total', totalWithTax)
+
+
+export {}
