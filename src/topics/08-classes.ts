b905b666dbdccb5cd77a81b8ebdfe3ea62167364
jolega <jolegacm@gmail.com>
2025-01-19 16:03:32 -0500

    inyeccion de dependencias

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -4,27 +4,62 @@ export class Person {
 
     constructor( 
         public name: string ,
+        public lastName: string,
         private address: string = 'No Address'
     ) {}
 
 }
 
 
-export class Hero extends Person {
+// export class Hero extends Person {
 
-    constructor(
-        public alterego: string,
-        public age: number,
-        public realName: string
+//     constructor(
+//         public alterego: string,
+//         public age: number,
+//         public realName: string
 
-    ){
-        super(realName, 'New York')
-    }
+//     ){
+//         super(realName, 'New York')
+//     }
 
-}
+// }
 
-const ironman1 = new Person ("Johan", "New York");
-const ironman2 = new Hero ("Johan", 45,'Tony');
+// export class Hero {
 
-console.log(ironman1)
+//     public person: Person;
+
+//     constructor(
+//         public alterego: string,
+//         public age: number,
+//         public realName: string
+
+//     ){
+//            this.person = new Person(realName) 
+//     }
+
+// }
+
+export class Hero {
+
+       
+    
+        constructor(
+            public alterego: string,
+            public age: number,
+            public realName: string,
+            public person: Person,
+    
+        ){
+        }
+    
+     }
+
+const tony = new Person ('Tony', 'Stark', 'New York')
+
+//const ironman1 = new Person ("Johan", "New York",tony);
+const ironman2 = new Hero ("Johan", 45,'Tony', tony);
+
+//console.log(ironman1)
 console.log(ironman2)
+
+
